aa44b921a453a34f0ac489f1057c80e7a8cdc4e1
James McCormac <jmccormac001@gmail.com>
2020-07-16 18:58:11 +0100

    adds missing _id on camera and chip

--- write_cotrendy_config_file.py
+++ write_cotrendy_config_file.py
@@ -29,7 +29,7 @@ def arg_parse():
     return p.parse_args()
 
 args = arg_parse()
-root = f"/tess/photometry/tessFFIextract/lightcurves/{args.sector_id}_{args.camera}-{args.chip}"
+root = f"/tess/photometry/tessFFIextract/lightcurves/{args.sector_id}_{args.camera_id}-{args.chip_id}"
 config_filename = f"{root}/config_{args.sector_id}_{args.camera_id}-{args.chip_id}.toml"
 config_template = f"""# This is a TOML config file for TESS Sector {args.sector_id}
 [owner]
